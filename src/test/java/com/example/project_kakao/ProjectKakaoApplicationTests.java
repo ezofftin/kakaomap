c0e3f0b2927ed7e8fbd8e9cbbd1b5b355dd1f6aa
ezofftin <ezofftin@gmail.com>
2024-01-20 19:49:25 +0900

    first commit

---
+++ src/test/java/com/example/project_kakao/ProjectKakaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_kakao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKakaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
